0326075fac559bfdabb4524a4be0c9a1c259196a
Franco Balich <franco.balich@gmail.com>
2023-03-10 00:28:15 -0300

    Add Profile icon component

--- src/TodoApp/components/ProfileIcon.jsx
+++ src/TodoApp/components/ProfileIcon.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import logo from  '../assets/Francolabs.png'
+import '../styles/Logo.css'
 
 export const ProfileIcon = () => {
   return (
-    <>
-      Icon
-    </>
+    <img className='logo' src={logo} alt="Logo de prueba" />
   )
 }
