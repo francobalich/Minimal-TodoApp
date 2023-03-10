49fc28f38a41786d429aeb1436e20b1e7961a066
Franco Balich <franco.balich@gmail.com>
2023-03-10 00:49:42 -0300

    Add container

--- src/TodoApp/components/ProfileIcon.jsx
+++ src/TodoApp/components/ProfileIcon.jsx
@@ -3,6 +3,8 @@ import '../styles/Logo.css'
 
 export const ProfileIcon = () => {
   return (
+  <div className="AppMenu">
     <img className='logo' src={logo} alt="Logo de prueba" />
+  </div>
   )
 }
